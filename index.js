c791164d16a4bd52c9ce7d7f4b8111adb6a386dc
Shree Vatsa <vastas2002@gmail.com>
2023-12-07 02:53:48 +0530

    added retreival

--- index.js
+++ index.js
@@ -1,60 +1,52 @@
 const axios = require("axios");
 const { RecursiveCharacterTextSplitter } = require("langchain/text_splitter");
-const {
-  CheerioWebBaseLoader,
-} = require("langchain/document_loaders/web/cheerio");
+// const {
+//   CheerioWebBaseLoader,
+// } = require("langchain/document_loaders/web/cheerio");
 
 const { OpenAIEmbeddings } = require("langchain/embeddings/openai");
 
-const { MemoryVectorStore } = require("langchain/vectorstores/memory");
+// const { MemoryVectorStore } = require("langchain/vectorstores/memory");
 
-const { RetrievalQAChain } = require("langchain/chains");
-const { ChatOpenAI } = require("langchain/chat_models/openai");
+const { Pinecone } = require("@pinecone-database/pinecone");
+const { PineconeStore } = require("langchain/vectorstores/pinecone");
+const { VectorDBQAChain } = require("langchain/chains");
 
-async function fetchWhat() {
-  try {
-    const loader = new CheerioWebBaseLoader(
-      "https://book.cairo-lang.org/ch02-01-variables-and-mutability.html"
-    );
+// const { RetrievalQAChain } = require("langchain/chains");
+// const { ChatOpenAI } = require("langchain/chat_models/openai");
+const { OpenAI } = require("langchain/llms/openai");
 
-    const joinedText = await loader.load();
+async function fetchWhat(question) {
+  // add all links
+  const linkArrays = [];
+  const pinecone = new Pinecone();
+  const index = pinecone.Index("cairo-pilot");
+  const embeddings = new OpenAIEmbeddings();
 
-    // console.log(codeBlocks);
-    // console.log(joinedText);
+  const vectorStore = await PineconeStore.fromExistingIndex(embeddings, {
+    pineconeIndex: index,
+  });
 
-    // Combine text and code
-    const textSplitter = new RecursiveCharacterTextSplitter({
-      chunkSize: 500,
-      chunkOverlap: 0,
-    });
+  console.log("vectorStore", vectorStore);
+  const model = new OpenAI({
+    modelName : "gpt-3.5-turbo"
+  });
+  const chain = VectorDBQAChain.fromLLM(model, vectorStore, {
+    k: 5,
+    returnSourceDocuments: true,
+  });
 
-    const splitDocs = await textSplitter.splitDocuments(joinedText);
+  const response = await chain.call({
+    query: question,
+  });
 
-    const embeddings = new OpenAIEmbeddings();
-
-    // change to pinecone-database from in memory and then check pinecone compatiblity with lanhcgain seeing the pinecone prompts and stroing using langchain
-    // ref : https://js.langchain.com/docs/integrations/vectorstores/pinecone
-    const vectorStore = await MemoryVectorStore.fromDocuments(
-      splitDocs,
-      embeddings
-    );
-
-    console.log(vectorStore);
-
-    // const relevantDocs = await vectorStore.similaritySearch("What is Cairo?");
-
-    // console.log(relevantDocs.length);
-    const model = new ChatOpenAI({ modelName: "gpt-3.5-turbo" });
-    const chain = RetrievalQAChain.fromLLM(model, vectorStore.asRetriever());
-
-    const response = await chain.call({
-      query: "How to declare a mutable variable in Cairo?",
-    });
-
-    console.log(response);
-  } catch (err) {
-    throw new Error(err);
-  }
+  console.log(response);
+  // change to pinecone-database from in memory and then check pinecone compatiblity with lanhcgain seeing the pinecone prompts and stroing using langchain
+  // ref : https://js.langchain.com/docs/integrations/vectorstores/pinecone
+  // const vectorStore = await MemoryVectorStore.fromDocuments(
+  //   splitDocs,
+  //   embeddings
+  // );
 }
 
 fetchWhat();
