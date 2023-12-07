bd4dfd14ee83d6c97ffbbe5224e5703ce434ff44
shabbiryk.eth <shabbirkhand.sk@gmail.com>
2023-12-07 09:10:37 +0530

    Added Data Fetching and Response Box

--- client/cairogptclient/src/App.jsx
+++ client/cairogptclient/src/App.jsx
@@ -1,10 +1,38 @@
 // src/App.js
-import React from "react";
+import React, { useState } from "react";
 import DataFetcher from "./dataFetcher";
 import "./App.css";
 
 function App() {
-  return <></>;
+  const [searchQuery, setSearchQuery] = useState("");
+  const [apiUrl, setApiUrl] = useState("");
+
+  const handleInputChange = (event) => {
+    setSearchQuery(event.target.value);
+  };
+
+  const handleSearchClick = () => {
+    // Build the API URL with query parameters when the search button is clicked
+    setApiUrl(
+      https://jsonplaceholder.typicode.com/posts?userId=${searchQuery || 1}
+    );
+  };
+
+  return (
+    <div className="container">
+      <h1>Cairo Co-Pilot</h1>
+      <div className="search-container">
+        <input
+          type="text"
+          placeholder="Expalin the constants of the cairo"
+          value={searchQuery}
+          onChange={handleInputChange}
+        />
+        <button onClick={handleSearchClick}>Search</button>
+      </div>
+      {apiUrl && <DataFetcher apiUrl={apiUrl} />}
+    </div>
+  );
 }
 
 export default App;
