f9d5cbc33598a939231c7229f6bc5c6bf3527a20
shabbiryk.eth <shabbirkhand.sk@gmail.com>
2023-12-07 09:12:47 +0530

    Added the Data Fetcher and Axios Client

--- client/cairogptclient/src/dataFetcher.jsx
+++ client/cairogptclient/src/dataFetcher.jsx
@@ -1,1 +1,32 @@
-// data fetching only
+// src/DataFetcher.js
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import "./App.css"; // Import the CSS file
+
+const DataFetcher = ({ apiUrl }) => {
+  const [data, setData] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await axios.get(apiUrl);
+        setData(response.data);
+      } catch (error) {
+        console.error("Error fetching data:", error);
+      } finally {
+        setLoading(false);
+      }
+    };
+
+    fetchData();
+  }, [apiUrl]);
+
+  return (
+    <div className="result-container">
+      {loading ? <p>Loading...</p> : <pre>{JSON.stringify(data, null, 2)}</pre>}
+    </div>
+  );
+};
+
+export default DataFetcher;
